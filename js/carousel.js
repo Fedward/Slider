b7cabf0dac9602ce3caf98c5c9f6ec77d3b8a059
Fedor <piknikfedor@gmail.com>
2018-11-12 13:52:30 +0300

    little changes

--- js/carousel.js
+++ js/carousel.js
@@ -1,61 +1,55 @@
-const slider = {
-	activeItems: 3, // кол-во элементов слайдера на экране, задает пользователь
-	everyItems: [],
+const carousel = {
+	visibleItems: 3, // кол-во элементов слайдера на экране, задает пользователь
+	totalItems: [],
 	setItemWidth: function () {
-		for (let i = 0; i < this.everyItems.length; i++) {
-			this.everyItems[i].setAttribute("style", "width: " + itemWidth + "%");
+		for (let i = 0; i < this.totalItems.length; i++) {
+			this.totalItems[i].setAttribute("style", "width: " + itemWidth + "%");
 		};		
-	};
+	}
 }
 
 let next, 
 	prev, 
 	scroll, 
-	scrollStep, 
-	itemWidth = Math.floor(1 / slider.activeItems * 100),
-	steps = 0;
-
-function makeStep () {
-	// по идее здесь еще должна быть куча префиксов для transform	
-	scroll.setAttribute("style", "transform: translate(-" + scrollStep * steps + "px, 0);");
-}
-
-/*function setItemWidth () {
-	for (let i = 0; i < slider.everyItems.length; i++) {
-		slider.everyItems[i].setAttribute("style", "width: " + itemWidth + "%");
-	};
-}	*/
+	stepLength, 
+	itemWidth = Math.round(1 / carousel.visibleItems * 100),
+	currentStep = 0;
 
 window.onload = function() {
 	next = document.getElementById("next");
 	prev = document.getElementById("prev");
 	scroll = document.getElementById("scroll");
-	slider.everyItems = document.getElementsByClassName("item");
-	slider.setItemWidth();
-	scrollStep = scroll.clientWidth / slider.activeItems;
+	carousel.totalItems = document.getElementsByClassName("item");
+	carousel.setItemWidth();
+	stepLength = scroll.clientWidth / carousel.visibleItems;
 
 	next.onclick = function () {
-		if (steps >= (slider.everyItems.length - slider.activeItems)) {			
-			steps = 0;
+		if (currentStep >= (carousel.totalItems.length - carousel.visibleItems)) {			
+			currentStep = 0;
 			makeStep();			
 		} else {
-			steps++;
+			currentStep++;
 			makeStep();
 		}
 	}
 
 	prev.onclick = function () {
-		if (steps == 0) {
-			steps = slider.everyItems.length - slider.activeItems;
+		if (currentStep == 0) {
+			currentStep = carousel.totalItems.length - carousel.visibleItems;
 			makeStep();
 		} else {
-			steps--;
+			currentStep--;
 			makeStep();
 		}				
 	}
 }
 
 window.onresize = function () {
-	scrollStep = scroll.clientWidth / slider.activeItems;
+	stepLength = scroll.clientWidth / carousel.visibleItems;
 	makeStep();
+}
+
+function makeStep () {
+	// по идее здесь можно добавить префиксы для transform	
+	scroll.setAttribute("style", "transform: translate(-" + stepLength * currentStep + "px, 0);");
 }
