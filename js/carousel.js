317a5dc7fda28c45c712334d53d513426dd69df8
Fedward <piknikfedor@gmail.com>
2019-01-12 09:49:30 +0300

    added carousel's visible items to UI

--- js/carousel.js
+++ js/carousel.js
@@ -1,18 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
 	'use strict';
-	let carouselInit = document.getElementById("carousel-init");
-		carouselInit.addEventListener("click", initCarousel);
+	let carouselInit = document.getElementById("carousel-init"),
+		visibleItems = document.getElementById('visible-items');
+		carouselInit.addEventListener("click", ()=>initCarousel(visibleItems.value));
 });
 
-function initCarousel() {
+function initCarousel(visibleItems) {
 	'use strict';
 	let carousel = {
 		carouselBlock: document.getElementById("carousel"),
-		wrapper: document.getElementById("carousel-wrapper"),		
-		visibleItems: 3, // кол-во элементов слайдера на экране, задает пользователь
+		wrapper: document.getElementById("carousel-wrapper"),
+		visibleItems, // slides amount
 		totalItems: document.getElementsByClassName("item"),
 		next: document.getElementById("carousel-next"),
-		prev: document.getElementById("carousel-prev"),	
+		prev: document.getElementById("carousel-prev"),
 		stepLength: 0,
 		currentStep: 0,
 		calcAllParameters: function () {
@@ -25,16 +26,16 @@ function initCarousel() {
 		},
 		makeStep: function () {
 			this.wrapper.style.transform = `translate(-${ this.stepLength * this.currentStep }px, 0)`;
-		},
-
+		}
 	}
 
 	carousel.calcAllParameters();
+	carousel.makeStep(); // for relaunch with different params
 
 	carousel.next.addEventListener('click', () => {
-		if (carousel.currentStep >= (carousel.totalItems.length - carousel.visibleItems)) {			
+		if (carousel.currentStep >= (carousel.totalItems.length - carousel.visibleItems)) {
 			carousel.currentStep = 0;
-			carousel.makeStep();			
+			carousel.makeStep();
 		} else {
 			carousel.currentStep++;
 			carousel.makeStep();
@@ -48,7 +49,7 @@ function initCarousel() {
 		} else {
 			carousel.currentStep--;
 			carousel.makeStep();
-		}				
+		}
 	});
 
 	window.addEventListener('resize', () => {
